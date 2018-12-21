950398bfb49fdd13533d48ab93900b9733fe146c
yanhk <705177961@qq.com>
2018-12-21 22:39:15 +0800

    增加 csrf 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,14 +7,26 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //用户注册
+    //用户注册表单
     public function create()
     {
         return view('users.create');
     }
 
+    //用户信息展示
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    //用户注册表单提交
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
