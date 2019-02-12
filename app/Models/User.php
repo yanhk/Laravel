1838fed4d6c6cef252ec0c4a9af0dc99cedc7960
Yanhk <705177961@qq.com>
2019-02-12 09:37:49 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -54,7 +55,12 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
+//        return $this->statuses()
+//            ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
             ->orderBy('created_at', 'desc');
     }
 
